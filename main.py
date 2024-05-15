bdf073355f4c600fd6a5d4cfeb4327e7abadd118
Hussein Hareb <88323940+husseinhareb@users.noreply.github.com>
2024-05-15 20:28:49 +0000

    bot template

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
-import discord
-from discord import message
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
 
-client = discord.Client()
+# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
+load_dotenv()
+TOKEN: Final[str] = os.getenv('TOKEN')
 
+# STEP 1: BOT SETUP
+intents: Intents = Intents.default()
+intents.message_content = True  # NOQA
+client: Client = Client(intents=intents)
+
+
+# STEP 2: MESSAGE FUNCTIONALITY
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled probably)')
+        return
+
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+# STEP 3: HANDLING THE STARTUP FOR The BOT
 @client.event
-async def on_ready():
-  print("We have logged in as {0.user}".format(client))
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
 
+
+# STEP 4: HANDLING INCOMING MESSAGES
 @client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
 
-  if message.content.startswith("!hello"):
-    await message.channel.send("Hello!")
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
 
-  if message.content.startswith("!help"):
-    await message.channel.send("This is a bot made")
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+
+# STEP 5: MAIN ENTRY POINT
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+if __name__ == '__main__':
+    main()
+
+
